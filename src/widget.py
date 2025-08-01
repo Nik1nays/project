2efec95d6dbb6c8b22afa5a971cab27e8912076c
Nikita Tysher <ntysher@mail.ru>
2025-08-01 17:08:00 +0300

    update widget

--- src/widget.py
+++ src/widget.py
@@ -1,17 +1,16 @@
-from masks import get_mask_account
-from masks import get_mask_card_number
+from masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(user_input: str) -> str:
     """Функция определяет тип и делает маскировку"""
     if "счет" in user_input.lower():
-        number_account = int(user_input[-20:])
-        return f"Счет {str(get_mask_account(number_account))}"
+        return f"Счет {get_mask_account(user_input[-20:])}"
     elif user_input[-16:].isdigit():
-        return f"{user_input[:-16]}{get_mask_card_number(int(user_input[-16:]))}"
+        return f"{user_input[:-16]}{get_mask_card_number(user_input[-16:])}"
     return "Неверно введены данные"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(mask_account_card("Maestro 1596837868705199"))
     print(mask_account_card("Счет 64686473678894779589"))
     print(mask_account_card("MasterCard 7158300734726758"))
@@ -26,5 +25,6 @@ def get_date(user_date: str) -> str:
     """Функция переделывает дату"""
     return f"{user_date[8:10]}.{user_date[5:7]}.{user_date[:4]}"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(get_date("2024-03-11T02:26:18.671407"))

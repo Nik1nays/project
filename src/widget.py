ef0f6218fa3ca9b7ca96dcfadf99305e3aa5efb1
Nik1nays <ntysher@mail.ru>
2025-09-21 19:07:51 +0300

    update get_date

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
@@ -10,21 +12,10 @@ def mask_account_card(user_input: str) -> str:
     return "Неверно введены данные"
 
 
-if __name__ == "__main__":
-    print(mask_account_card("Maestro 1596837868705199"))
-    print(mask_account_card("Счет 64686473678894779589"))
-    print(mask_account_card("MasterCard 7158300734726758"))
-    print(mask_account_card("Счет 35383033474447895560"))
-    print(mask_account_card("Visa Classic 6831982476737658"))
-    print(mask_account_card("Visa Platinum 8990922113665229"))
-    print(mask_account_card("Visa Gold 5999414228426353"))
-    print(mask_account_card("Счет 73654108430135874305"))
+def get_date(date_str: str) -> str:
+    date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
+    return date_obj.strftime("%d.%m.%Y")
 
 
-def get_date(user_date: str) -> str:
-    """Функция переделывает дату"""
-    return f"{user_date[8:10]}.{user_date[5:7]}.{user_date[:4]}"
 
 
-if __name__ == "__main__":
-    print(get_date("2024-03-11T02:26:18.671407"))

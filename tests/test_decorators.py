dd26b22a471c7d772faf6a8100176d8aceea96b0
Nik1nays <ntysher@mail.ru>
2025-10-15 13:43:19 +0300

    new decorators

---
+++ tests/test_decorators.py
@@ -0,0 +1,21 @@
+import pytest
+
+from src.decorators import my_function, log
+
+# функция для теста
+def divide_num(a, b):
+    return a / b
+
+
+@log()
+def test_my_function(capsys):
+    result = my_function(1, 2)
+    captured = capsys.readouterr()
+    assert result == 3
+    assert "Функцияmy_function" in captured.out
+
+
+@log()
+def test_err_function():
+    with pytest.raises(ZeroDivisionError):
+        divide_num(1, 0)
